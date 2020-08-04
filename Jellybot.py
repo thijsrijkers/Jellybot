5c83400e6f12e29339b9e3c8643477d3bfc81b1a
thijsrijkers <thijsrijkers@live.nl>
2020-08-05 01:07:39 +0200

    Bot can respond to messages and will interact if you call him. BUT the import doesnt work atm so the code block that is comment can maybe not work

---
+++ Jellybot.py
@@ -0,0 +1,50 @@
+import praw
+from imageai.Detection import ObjectDetection
+#[TODO: Personfix: Fix the import of objectdetection to the right version]
+
+#reddit login for API
+reddit = praw.Reddit(   client_id="F7559eYxDl7KPQ",
+                        client_secret="xFmvRbrtfkQAkLiHNJnLAQe3V4g",
+                        password="Jellybot_",
+                        user_agent="Jellybot was created by u/oocryoo",
+                        username="Jellybot_"
+                    )
+
+print(reddit.user.me())
+
+#Giving the subreddits that the bot is allowed to use [TODO: make it a array or a list or something lmao]
+subreddit = reddit.subreddit('testingground4bots')
+
+#Call to the bot
+keyphrase = '!jellycheck'
+
+# looks if the keyphrase is called in the comments
+for comment in subreddit.stream.comments():
+    if keyphrase in comment.body:
+        print('Comment found!')
+        try:
+                #############################################
+                #[TODO: Fix this code block after the import update is fixed]
+                submission = comment.submission
+                url = submission.url
+
+                detector = ObjectDetection()
+
+                model_path = url
+                input_path = url
+                output_path = url
+
+                detector.setModelTypeAsTinyYOLOv3()
+                detector.setModelPath(model_path)
+                detector.loadModel()
+                detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
+
+                for eachItem in detection:
+                    print(eachItem["name"] , " : ", eachItem["percentage_probability"])
+                #############################################
+
+                reply = 'Thnx for using Jellybot! ATM were still working on the repost detection functions.'
+                comment.reply(reply)
+                print('posted')
+        except:
+            print('to frequent')
